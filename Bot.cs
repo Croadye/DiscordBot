8e00473d789b22f34d0cb6d0fb9b040146f24a9a
Croadye <105956066+Croadye@users.noreply.github.com>
2022-05-20 18:47:50 -0700

    Add files via upload

---
+++ Bot.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeBot
+{
+    class Bot
+    {
+        public DiscordSocketClient client { get; private set; }
+        private ChannelConnector channel;
+        private MediaPlayer mediaPlayer;
+        private CommandHandler commandHandler;
+
+        public async Task MainAsync()
+        {
+            //Reads Json File 
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config/config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+            json = await sr.ReadToEndAsync().ConfigureAwait(false);
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+            //Get Token From Json File
+            var token = configJson.Token;
+            var prefix = configJson.Prefix;
+
+            //Creats Bot
+            client = new DiscordSocketClient();
+            client.Log += Log;
+
+
+            mediaPlayer = new MediaPlayer();
+            channel = new ChannelConnector(mediaPlayer);
+
+            commandHandler = new CommandHandler(prefix, client, mediaPlayer);
+
+            //Adds Events
+            client.UserVoiceStateUpdated += channel.OnVoiceStateUpdated;
+            client.MessageReceived += commandHandler.CommandHandle;
+
+            //Starts Up The Bot
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Media.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace WelcomeBot
+{
+    class Media
+    {
+        string[] listOfSounds;
+        int lastPlayed = -1;
+
+        public Media() 
+        {
+            string path = "sounds";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public string GetRandSound()
+        {
+            return GenerateRandomSound();
+        }
+
+        private string GenerateRandomSound()
+        {
+            var rand = new Random();
+            int randNum = rand.Next(listOfSounds.Length);
+            if (randNum != lastPlayed)
+            {
+                lastPlayed = randNum;
+                return listOfSounds[randNum];
+            } else
+            {
+                if (randNum == (listOfSounds.Length - 1)) randNum -= 1;
+                else randNum += 1;
+                lastPlayed = randNum;
+                return listOfSounds[randNum];
+            }
+        }
+
+        public void PopulateSoundArray(String path)
+        {
+            listOfSounds = Directory.GetFiles(path);
+        }
+
+        public String[] GetSoundArray()
+        {
+            return listOfSounds;
+        }
+    }
+}
